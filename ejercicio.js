0db451a182f0aa4715b2057e6c12fa3437626030
Amauta20 <schunga@gmail.com>
2023-08-04 22:16:40 -0500

    Cambio javascript

--- ejercicio.js
+++ ejercicio.js
@@ -1,12 +1,13 @@
 console.log("Hola Mundo")
 
 var myDate = new Date();
-    var hrs = myDate.getHours();
-    var saludar;
+var hrs = myDate.getHours();
+var saludar;
     if (hrs < 12)
         saludar = 'Buenos días';
     else if (hrs >= 12 && hrs <= 18)
         saludar = 'Buenas tardes';
     else if (hrs >= 18 && hrs <= 24)
         saludar = 'Buenas noches';
-    document.getElementById('saludo').innerHTML = saludar;
+
+document.getElementById('saludo').innerHTML = saludar;
