19aa3cd3cdfd47096ab5fce01df396f01cea11a3
Amauta20 <schunga@gmail.com>
2023-08-04 21:45:44 -0500

    Agregue funcion js

--- ejercicio.js
+++ ejercicio.js
@@ -1,1 +1,12 @@
 console.log("Hola Mundo")
+
+var myDate = new Date();
+    var hrs = myDate.getHours();
+    var saludar;
+    if (hrs < 12)
+        saludar = 'Buenos días';
+    else if (hrs >= 12 && hrs <= 18)
+        saludar = 'Buenas tardes';
+    else if (hrs >= 18 && hrs <= 24)
+        saludar = 'Buenas noches';
+    document.getElementById('saludo').innerHTML = saludar;
